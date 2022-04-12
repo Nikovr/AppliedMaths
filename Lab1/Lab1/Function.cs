cf272b99a2251b31721f062484c1bddb4f66e4e9
Nikolai Kovregin <kol89320@hotmail.com>
2022-04-12 23:08:46 +0300

    added dichotomy method and implemented our mathematical function

---
+++ Lab1/Lab1/Function.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1
+{
+    public class Function
+    {
+        public static double GetValue(double x)
+        {
+            return Math.Sin(x * Math.PI / 180) * Math.Pow(x, 2);
+        }
+    }
+}
---
+++ Lab1/Lab1/Optimization Methods/Dichotomy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1
+{
+    public static class Dichotomy 
+    {
+        public static double Start(double accuracy, double leftPoint, double rightPoint)
+        {
+            while (Math.Abs(leftPoint - rightPoint) > accuracy)
+            {
+                if (Function.GetValue(leftPoint) < Function.GetValue(rightPoint))
+                {
+                    rightPoint = (leftPoint + rightPoint) / 2;
+                }
+                else
+                {
+                    leftPoint = (leftPoint + rightPoint) / 2;
+                }
+            }
+
+            return (leftPoint + rightPoint) / 2;
+        }
+    }
+}
