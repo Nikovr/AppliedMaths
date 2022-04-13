9742d6ec7d7e4067b2f09ae9e58100d7bd887b95
Nikolai Kovregin <kol89320@hotmail.com>
2022-04-13 18:43:01 +0300

    updated dichotomy to have fewer function calls

--- Lab1/Lab1/Optimization Methods/Dichotomy.cs
+++ Lab1/Lab1/Optimization Methods/Dichotomy.cs
@@ -9,15 +9,20 @@ namespace Lab1.Optimization_Methods
         }
         public double Start(double accuracy, double leftPoint, double rightPoint)
         {
+            double leftValue = Function.GetValue(leftPoint);
+            double rightValue = Function.GetValue(rightPoint);
+
             while (Math.Abs(leftPoint - rightPoint) > accuracy)
             {
-                if (Function.GetValue(leftPoint) < Function.GetValue(rightPoint))
+                if (leftValue < rightValue)
                 {
                     rightPoint = (leftPoint + rightPoint) / 2;
+                    rightValue = Function.GetValue(rightPoint);
                 }
                 else
                 {
                     leftPoint = (leftPoint + rightPoint) / 2;
+                    leftValue = Function.GetValue(leftPoint);
                 }
             }
 
---
+++ Lab1/Lab1/Optimization Methods/GoldenSection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1.Optimization_Methods
+{
+    public class GoldenSection
+    {
+    }
+}
