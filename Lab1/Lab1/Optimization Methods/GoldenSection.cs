52d1c11a297e28da492995cf5f9f03d0783d10b2
Nikolai Kovregin <kol89320@hotmail.com>
2022-04-13 19:58:01 +0300

    added golden section method

--- Lab1/Lab1/Optimization Methods/GoldenSection.cs
+++ Lab1/Lab1/Optimization Methods/GoldenSection.cs
@@ -6,5 +6,41 @@ namespace Lab1.Optimization_Methods
 {
     public class GoldenSection
     {
+        private readonly double GoldenRatio = (1 + Math.Sqrt(5)) / 2;
+
+        public GoldenSection()
+        {
+        }
+
+        public double Start(double accuracy, double leftPoint, double rightPoint)
+        {
+            double left = rightPoint - Math.Abs(leftPoint - rightPoint) / GoldenRatio;
+            double right = leftPoint + Math.Abs(leftPoint - rightPoint) / GoldenRatio;
+
+            double leftValue = Function.GetValue(left);
+            double rightValue = Function.GetValue(right);
+
+            while (Math.Abs(leftPoint - rightPoint) > accuracy)
+            {
+                if (leftValue < rightValue)
+                {
+                    rightPoint = right;
+                    right = left;
+                    rightValue = leftValue;
+                    left = rightPoint - Math.Abs(leftPoint - rightPoint) / GoldenRatio;
+                    leftValue = Function.GetValue(left);
+                }
+                else
+                {
+                    leftPoint = left;
+                    left = right;
+                    leftValue = rightValue;
+                    right = leftPoint + Math.Abs(leftPoint - rightPoint) / GoldenRatio;
+                    rightValue = Function.GetValue(right);
+                }
+            }
+
+            return (leftPoint + rightPoint) / 2;
+        }
     }
 }
--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -11,10 +11,13 @@ namespace Lab1
             Function func = new Function();
             Fibonacci fib = new Fibonacci();
             Dichotomy dichotomy = new Dichotomy();
+            GoldenSection goldenSection = new GoldenSection();
             double res1 = fib.Start(0.0001, 1, 10);
             double res2 = dichotomy.Start(0.0001, 1, 10);
+            double res3 = goldenSection.Start(0.0001, 1, 10);
             Console.WriteLine(res1);
             Console.WriteLine(res2);
+            Console.WriteLine(res3);
             //to be continued
         }
     }
